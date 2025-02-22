2d090feee26780d217fa249f03c14b3468e8afec
David Weichselbaum <david.t.weichselbaum@gmail.com>
2025-02-22 13:56:40 +0100

    Add table for classifier head metrics

--- train.py
+++ train.py
@@ -5,11 +5,13 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from torchsummary import summary
 import wandb
+import pandas as pd
 
 from model import GreedyClassifier
 
 
-def train(model, train_loader, val_loader, optimizers, criterion, device, num_epochs=5):
+def train(model, train_loader, val_loader, optimizers, criterion, device, num_epochs):
+    outputs_df = pd.DataFrame(columns=["Steps", "Output", "Val Loss", "Val Accuracy"])
     model.train()
     # wandb.watch(model, log="all")  # Log gradients and model parameters
 
@@ -34,7 +36,7 @@ def train(model, train_loader, val_loader, optimizers, criterion, device, num_ep
             if total_steps % wandb.config.log_steps == 0:
                 train_loss /= train_accumulation_steps
                 train_accuracy /= train_accumulation_steps
-                val_loss, val_accuracy = validate(model, val_loader, criterion, device)
+                val_loss, val_accuracy, outputs_df = validate(model, val_loader, criterion, device, total_steps, outputs_df)
                 wandb.log({
                     "Steps": total_steps,
                     "Epoch": epoch + 1,
@@ -62,7 +64,23 @@ def get_accuracy(output, target):
     return accuracy
 
 
-def log_validation_bars(losses, accuracies):
+def log_outputs_table(total_steps, losses, accuracies, outputs_df):
+    new_rows = []
+    for output_idx, (loss, accuracy) in enumerate(zip(losses, accuracies)):
+        new_row = {
+            "Steps": total_steps,
+            "Output": output_idx,
+            "Val Loss": loss,
+            "Val Accuracy": accuracy
+        }
+        new_rows.append(new_row)
+    new_df = pd.DataFrame(new_rows)
+    outputs_df = pd.concat([outputs_df, new_df], ignore_index=True)
+    wandb.log({"Classifier Head Metrics": wandb.Table(dataframe=outputs_df), "Steps": total_steps})
+    return outputs_df
+
+
+def log_outputs_plot(total_steps, losses, accuracies):
     output_labels = [f"Output {i+1}" for i in range(len(losses))]
 
     losses_data = [[label, loss] for label, loss in zip(output_labels, losses)]
@@ -73,10 +91,12 @@ def log_validation_bars(losses, accuracies):
     accuracies_table = wandb.Table(data=accuracies_data, columns=["Output", "Accuracy"])
     accuracies_plot = wandb.plot.bar(accuracies_table, "Output", "Accuracy", title="Validation Accuracies for Each Output")
 
-    wandb.log({"Validation Accuracies": accuracies_plot, "Validation Losses": losses_plot})
+    wandb.log(
+        {"Validation Accuracies": accuracies_plot, "Validation Losses": losses_plot},
+        step=total_steps)
 
 
-def validate(model, val_loader, criterion, device):
+def validate(model, val_loader, criterion, device, total_steps, outputs_df):
     val_losses = []
     val_accuracies = []
     model.eval()
@@ -99,9 +119,29 @@ def validate(model, val_loader, criterion, device):
     avg_val_accuracies = [sum(accs) / len(accs) for accs in val_accuracies]
 
     if len(avg_val_losses) > 1:
-        log_validation_bars(avg_val_losses, avg_val_accuracies)
+        outputs_df = log_outputs_table(total_steps, avg_val_losses, avg_val_accuracies, outputs_df)
+        log_outputs_plot(total_steps, avg_val_losses, avg_val_accuracies)
 
-    return avg_val_losses[-1], avg_val_accuracies[-1]
+    return avg_val_losses[-1], avg_val_accuracies[-1], outputs_df
+
+
+def init_optimizers(model):
+    optimizers = []
+    for i, (layer, classifier) in enumerate(zip(model.layers, model.classifiers)):
+        print(f"Layer {i+1}")
+        if not model.do_auxloss and i == len(model.layers) - 1:  # final classifier. if no auxloss, last optimizers gets all layers and last classifier
+            parameters = list(model.layers.parameters()) + list(model.classifiers[-1].parameters())
+            print(f"Parameters final classifier: {sum(p.numel() for p in parameters)}")
+        elif not model.do_auxloss:  # linear probes
+            parameters = list(classifier.parameters())
+            print(f"Parameters linear probe: {sum(p.numel() for p in parameters)}")
+        else:  #  auxiliary classifier. Gets all parameter of single layer
+            parameters = list(layer.parameters()) + list(classifier.parameters())
+            print(f"Parameters auxiliary classifier: {sum(p.numel() for p in parameters)}")
+
+        optimizer = optim.Adam(parameters, lr=wandb.config.learning_rate)
+        optimizers.append(optimizer)
+    return optimizers
 
 
 def run():
@@ -127,25 +167,10 @@ def run():
     model.to(device)
     summary(model, input_size=(3, 32, 32))
 
-    optimizers = []
-    for i, (layer, classifier) in enumerate(zip(model.layers, model.classifiers)):
-        print(f"Layer {i+1}")
-        if not model.do_auxloss and i == len(model.layers) - 1:  # final classifier. if no auxloss, last optimizers gets all layers and last classifier
-            parameters = list(model.layers.parameters()) + list(model.classifiers[-1].parameters())
-            print(f"Parameters final classifier: {sum(p.numel() for p in parameters)}")
-        elif not model.do_auxloss:  # linear probes
-            parameters = list(classifier.parameters())
-            print(f"Parameters linear probe: {sum(p.numel() for p in parameters)}")
-        else:  #  auxiliary classifier. Gets all parameter of single layer
-            parameters = list(layer.parameters()) + list(classifier.parameters())
-            print(f"Parameters auxiliary classifier: {sum(p.numel() for p in parameters)}")
-
-        optimizer = optim.Adam(parameters, lr=wandb.config.learning_rate)
-        optimizers.append(optimizer)
-
+    optimizers = init_optimizers(model)
     criterion = nn.CrossEntropyLoss()
 
-    train(model, train_loader, val_loader, optimizers, criterion, device, num_epochs=wandb.config.epochs)
+    train(model, train_loader, val_loader, optimizers, criterion, device, wandb.config.epochs)
 
 
 if __name__ == "__main__":
@@ -153,7 +178,7 @@ if __name__ == "__main__":
         # mode="disabled",
         project="greedy_learning_test_CIFAR10",
         group="test",
-        name="test2_+auxloss_-gradients_-residuals",
+        name="test4_-auxloss_+gradients_-residuals",
         config={
             "epochs": 20,
             "batch_size": 64,
